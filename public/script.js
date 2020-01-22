a2e3e38be251209bdb1387fd662cbb9bd72ca8f7
Ryan Patterson <cgamesplay@cgamesplay.com>
2020-01-22 12:47:41 +0400

    Fix LinkedIn icon and add copy url

--- public/script.js
+++ public/script.js
@@ -10,4 +10,13 @@ $(function() {
   stickyNav();
   $(window).on("resize", stickyNav);
   $(window).scroll(stickyNav);
-})();
+});
+
+function copyText(text) {
+  const el = document.createElement("textarea");
+  el.value = text;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+}
